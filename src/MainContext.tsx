c50208a45dc93b150b6a40a438b3cd4356d7c330
Michael <Mikle_Bond@mail.ru>
2023-02-20 17:18:53 +0300

    Maybe fix promises

--- src/MainContext.tsx
+++ src/MainContext.tsx
@@ -531,7 +531,7 @@ export const MainProvider = ({ children }) => {
     const getSettings = async () => {
       //fall back to localstorage for legacy settings
       let fallback = false;
-      function loaderForLegacySetting(name: string, setter: Function, prefer: boolean = true): Promise {
+      function loaderForLegacySetting(name: string, setter: Function, prefer: boolean = true): Promise<void> {
         return async () => {
           let saved = await localForage.getItem(name);
           if (saved !== null) {
@@ -621,7 +621,7 @@ export const MainProvider = ({ children }) => {
         vidFilter: true,
       };
 
-      function loaderForLegacyFilter (name: string, setter: Function): Promise {
+      function loaderForLegacyFilter (name: string, setter: Function): Promise<void> {
         return async () => {
           let saved_value = await localForage.getItem(name);
           if (saved_value !== null) {
@@ -654,7 +654,7 @@ export const MainProvider = ({ children }) => {
       const loadVidFilter = loaderForLegacyFilter("vidFilter", setVidFilter);
 
       //new setting
-      function loaderForFilter (name: string, setter: Function): Promise {
+      function loaderForFilter (name: string, setter: Function): Promise<void> {
         return async () => {
           let saved = await localForage.getItem(name);
           if (saved === false) {
@@ -670,7 +670,7 @@ export const MainProvider = ({ children }) => {
       const loadNsfwPostFilter = loaderForFilter("nsfwPostFilter", setNsfwPostFilter);
 
       //new settings don't need localstorage fallback..
-      function loaderForSettingBool(name: string, setter: Function, fallback: boolean): Promise {
+      function loaderForSettingBool(name: string, setter: Function, fallback: boolean): Promise<void> {
         return async () => {
           let saved = await localForage.getItem(name);
           saved === !fallback
